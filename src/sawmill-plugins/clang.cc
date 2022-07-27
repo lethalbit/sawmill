c610f0d8cefdbfa96e685df9aac5bec43215d17b
Aki Van Ness <aki@lethalbit.net>
2022-07-27 06:24:23 -0400

    sawmill: plugins: Refactored how compiler plugins are orginized

--- src/sawmill-plugins/clang.cc
+++ src/sawmill-plugins/clang.cc
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* clang/plugin.cc - Sawmill instrumentation plugin for clang */
 
+#include <limits>
+
 #include <clang/AST/ASTContext.h>
 #include <clang/AST/Attr.h>
 #include <clang/Sema/ParsedAttr.h>
@@ -11,7 +13,8 @@
 struct SawmillAttrInfo : public clang::ParsedAttrInfo {
 
 	SawmillAttrInfo() {
-		OptArgs = 15;
+		/* This produced an overflow warning because LLVM is using bitfields,,,, */
+		OptArgs = std::numeric_limits<decltype(OptArgs)>::max();
 
 		static constexpr Spelling attrs[] = {
 			{clang::ParsedAttr::AS_CXX11, "sawmill::instrument"}
