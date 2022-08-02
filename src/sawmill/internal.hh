d79b85aab86970763e562c42dc9da7765e7e881e
Aki Van Ness <aki@lethalbit.net>
2022-08-02 03:13:32 -0400

    sawmill: internal: added a multi-reader single writer mutex wrapper for arbitrary types

--- src/sawmill/internal.hh
+++ src/sawmill/internal.hh
@@ -13,9 +13,50 @@
 #define SAWMILL_WEAK_LINKAGE [[gnu::weak]]
 #endif
 
+#include <utility>
+#include <mutex>
+#include <shared_mutex>
+
 namespace sawmill::internal {
 
+	/*! Simple Read-Write lock based on std::shared_mutex */
+	template<typename T>
+	struct rwlock final {
+	private:
+		template<typename U, template<typename> typename lock_t>
+		struct lock_result final {
+		private:
+			lock_t<std::shared_mutex> _lock;
+			U& _obj;
+		public:
+			constexpr lock_result(std::shared_mutex& mut, U& obj) noexcept :
+				_lock{mut}, _obj{obj} { /* nop */ }
 
+			U* operator->() noexcept { return &_obj; }
+
+			U& operator*() noexcept { return _obj; }
+
+		};
+		std::shared_mutex _mutex{};
+		T _obj;
+
+	public:
+		/*! Forwarding constructor for T */
+		template<typename ...args_t>
+		constexpr rwlock(args_t&&... args) noexcept :
+			_obj{std::forward<args_t>(args)...} { /* nop */ }
+
+		/*! Return a const ref version of the object
+			to ensure that you can not change the result.
+		*/
+		lock_result<const T, std::shared_lock> read() noexcept {
+			return {_mutex, _obj};
+		}
+
+		lock_result<T, std::unique_lock> write() noexcept {
+			return {_mutex, _obj};
+		}
+	};
 }
 
 
